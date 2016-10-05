ce83f04dedc063b2222c29c457e7cf2c7e0f1957
Darkuit <abraham.andrade651@gmail.com>
2016-10-05 10:37:44 -0500

    Añ

--- claselistas2.py
+++ claselistas2.py
@@ -9,4 +9,10 @@ def sustituir0(x):
         z+=1
     print('Lista original: ', x,' lista resultado: ', y)
 
+x=[-22, 4, -3]
+sustituir0(x)
+y=[-1, -2, 788, 12, 45, 76, -21, 85, 123, 3, -2, 234, 746, 7346, 1234, 34, 7897,231,-34, 1]
+sustituir0(y)
+z=[-3,45, -2, 8]
+sustituir0(z)
     
