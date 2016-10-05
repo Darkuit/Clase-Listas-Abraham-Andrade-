688872c6064f7ff1057bcca991be01ba80ddb239
Darkuit <abraham.andrade651@gmail.com>
2016-10-05 10:52:48 -0500

    Añadidos los llamados

--- claselistas3.py
+++ claselistas3.py
@@ -9,5 +9,9 @@ def delpar(x):
             print('Eliminando el elemento', x[z],' con indice: ', z)
         z+=1
         
-    return y
-
+x= [235, 346, 12, 765,87, 97]
+delpar(x)
+y=[978, 5, 8998 , 97, 654, 64, 97, 4659, -89, 123, 45, 234, 76, 235, 87, 253, 765, 23, 76, 25]
+delpar(y)
+z=[2, 134, 123, 34, 87, 9]
+delpar(z)
