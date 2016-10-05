e0d80b0d113c9120f2cf6fc3594c425c6001b3bc
Darkuit <abraham.andrade651@gmail.com>
2016-10-05 10:31:17 -0500

    Añadidos los llamados

--- claselistas1.py
+++ claselistas1.py
@@ -6,3 +6,9 @@ def Maxlista(x):
             maxlista=x[z]
         z+=1
     print('El máximo número de la lista es: ',maxlista)
+x=[1, 4, 3]
+Maxlista(x)
+z=[5, 8 , 16, 24, 9, 7, 18, 6, 25, 45, 164, 9, 20, 214, 87, 3014, 56, 24, 19, 22]
+Maxlista(z)
+y=[8, 42, 23, 25]
+Maxlista(y)
